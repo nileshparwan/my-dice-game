de6cd72f5534e1fe207aa85ed5be865738459339
KoshalParwan <koshal.parwan@fasst.io>
2023-10-06 21:39:28 +0400

    [component][dice] create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dice from './components/Dice/Dice';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Dice />
     </div>
   );
 }
---
+++ src/components/Dice/Dice.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Dice.css';
+
+const Dice = () => {
+  return (
+    <div>Dice</div>
+  )
+}
+
+export default Dice;
