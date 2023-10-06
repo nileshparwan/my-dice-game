5225cd0c5db8d0f9f745f9e7fd839e5153b62716
KoshalParwan <koshal.parwan@fasst.io>
2023-10-06 23:47:50 +0400

    [components][dice] add roll dice functionality

--- src/components/Dice/Dice.tsx
+++ src/components/Dice/Dice.tsx
@@ -1,17 +1,39 @@
-import React, { ReactNode, useState } from 'react';
+import React, { KeyboardEventHandler, MouseEventHandler, useState } from 'react';
 import './Dice.css';
 import DiceImages from '../DiceImages/DiceImages';
 
 const Dice: React.FunctionComponent = () => {
     const diceSides = 2;
     const randomNumber = () => Math.ceil(Math.random() * diceSides);
-    
+
     const [state, setState] = useState({
         firstDiceDigit: randomNumber(),
         secondDiceDigit: randomNumber()
     });
 
     const { firstDiceDigit, secondDiceDigit } = state;
+
+    const rollDice = () => {
+        setState((prev) => ({
+            ...prev,
+            firstDiceDigit: randomNumber(),
+            secondDiceDigit: randomNumber()
+        }))
+    }
+
+    const buttonHandler:MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.preventDefault();
+        rollDice();
+    }
+
+    const keyPressHandler: KeyboardEventHandler<HTMLButtonElement> = (event) => {
+        event.preventDefault();
+        if (
+            event.key === "Enter"
+        ) {
+            rollDice();
+        }
+    }
 
     return (
         <div className="dice-container">
@@ -22,7 +44,12 @@ const Dice: React.FunctionComponent = () => {
 
             <p aria-live="polite">1</p>
 
-            <button className="btn" aria-label="Lancer le dés">
+            <button
+                className="btn"
+                aria-label="Lancer le dés"
+                onClick={buttonHandler}
+                onKeyDown={keyPressHandler}
+            >
                 Lancer le dés
             </button>
         </div>
