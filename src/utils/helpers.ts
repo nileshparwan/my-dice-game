27bc1d5a12132cfac047fa77fb4ca88de0a51607
KoshalParwan <koshal.parwan@fasst.io>
2023-10-08 20:49:12 +0400

    [utils] add helper method for random number and player

---
+++ src/utils/helpers.ts
@@ -0,0 +1,2 @@
+export const randomNumber = (maxNumber: number): number => Math.ceil(Math.random() * maxNumber);
+export const randomPlayer = (maxPlayer: number): number => Math.ceil(Math.random() * maxPlayer);
