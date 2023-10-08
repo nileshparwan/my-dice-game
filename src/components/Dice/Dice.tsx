47c30f479ddba8fc934f6ccc72975b8645c8827b
KoshalParwan <koshal.parwan@fasst.io>
2023-10-08 17:15:44 +0400

    [components][dice] alternate between players and calculate scores

--- src/components/Dice/Dice.tsx
+++ src/components/Dice/Dice.tsx
@@ -14,17 +14,31 @@ const Dice: React.FunctionComponent = () => {
     });
 
     const {diceValues, scores, currentPlayer} = state;
+    console.log(scores)
 
     const rollDice = () => {
-        setState((prev) => ({
-            ...prev,
-            firstDiceDigit: randomNumber(),
-            secondDiceDigit: randomNumber(),
-            startGame: true
-        }))
+        // set new dice value
+        const dice1 = randomNumber();
+        const dice2 = randomNumber(); 
+        const total = dice1 + dice2;
+        const playerScore = () => {
+            const newScore = scores;
+            const playerIndex = currentPlayer - 1;
+            const currentPlayerScore = newScore[playerIndex]; 
+            newScore[playerIndex] = currentPlayerScore + total;
+            return newScore;
+        }
+        const newPlayerScore = playerScore();
+        const nextPlayer = currentPlayer === 1 ? 2 : 1;
+        setState(prev => ({ 
+            ...prev, 
+            diceValues: [dice1, dice2],
+            scores: newPlayerScore,
+            currentPlayer: nextPlayer
+        }));
     }
 
-    const buttonHandler:MouseEventHandler<HTMLButtonElement> = (event) => {
+    const buttonHandler: MouseEventHandler<HTMLButtonElement> = (event) => {
         event.preventDefault();
         rollDice();
     }
